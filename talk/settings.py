40e0469fd7cc2a629c0dd781243bf0a026cff9b6
ayush jindal <jindalayush77@gmail.com>
2022-04-18 23:35:24 +0530

    make it better

--- talk/settings.py
+++ talk/settings.py
@@ -50,21 +50,21 @@ INSTALLED_APPS = [
 ASGI_APPLICATION="talk.asgi.application"
 
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [("127.0.0.1", 6379)],
-#         },
-#     },
-# }
-
-
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
+
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels.layers.InMemoryChannelLayer"
+#     }
+# }
 
 
 MIDDLEWARE = [

fb22917d30ef27a6379c35a4945162177318d79a
ayush jindal <jindalayush77@gmail.com>
2022-04-26 14:47:08 +0530

    new

--- talk/settings.py
+++ talk/settings.py
@@ -62,6 +62,15 @@ CHANNEL_LAYERS = {
         },
     },
 }
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get('REDIS_URL'),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
 
 # CACHES = {
 #     "default": {

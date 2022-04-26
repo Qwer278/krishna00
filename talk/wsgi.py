09708174b4310d8b8f84ad49bc01e99f1073da2e
ayush jindal <jindalayush77@gmail.com>
2022-04-26 14:20:23 +0530

    new

--- talk/wsgi.py
+++ talk/wsgi.py
@@ -13,5 +13,5 @@ import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talk.settings')
-django.setup()
+# django.setup()
 application = get_wsgi_application()

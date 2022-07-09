3576ee4b4da7fe859b560ce0b64f6af007a2cd37
Ayush Jindal(Jindalayush987@gmal.com) <94276821+Qwer278@users.noreply.github.com>
2022-07-10 00:45:05 +0530

    make it better

--- talk/settings.py
+++ talk/settings.py
@@ -57,7 +57,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": ["redis-10182.c299.asia-northeast1-1.gce.cloud.redislabs.com:10182"],
         },
     },
 }

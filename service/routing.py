1a90f0f4146d58556afc2fc850de903e5e3d6805
Ayush Jindal(Jindalayush987@gmal.com) <94276821+Qwer278@users.noreply.github.com>
2022-07-10 15:32:41 +0530

    make it better

--- service/routing.py
+++ service/routing.py
@@ -3,5 +3,5 @@ from . import consumers
 from .consumers import ChatConsumer
 
 websocket_urlpattern=[
-    path(r'ws/chat/',ChatConsumer.as_asgi()),
+    url(r'^ws/chat/$',ChatConsumer.as_asgi()),
 ]   

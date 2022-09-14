3a8eaf098a01a5b49f27c12c0d9f11cf461e0d03
Ayush Jindal(Jindalayush987@gmal.com) <94276821+Qwer278@users.noreply.github.com>
2022-09-15 00:45:04 +0530

    make it better

--- service/urls.py
+++ service/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path,include
 
 from . import views
-from routing import websocket_urlpattern
+import routing
 urlpatterns = [
     path('', views.homepage, name='index'),
     path('text/', views.text ,name="home"),
     path('text/chat', views.texting ,name="texting"),
     path('text/ip', views.ip_check ,name="ip"),
-    path('', include('websocket_urlpattern'))
+    path('', include('routing.websocket_urlpattern'))
 ]

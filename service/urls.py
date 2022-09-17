2cc3db371fd6db575231319338de9c73a226c64c
Ayush Jindal(Jindalayush987@gmal.com) <94276821+Qwer278@users.noreply.github.com>
2022-09-17 23:12:18 +0530

    make it better

--- service/urls.py
+++ service/urls.py
@@ -5,7 +5,7 @@ from . import views
 from . import routing
 urlpatterns = [
     path('', views.homepage, name='index'),
-    path('text/', views.text ,name="home"),
+    # path('text/', views.text ,name="home"),
     path('text/chat', views.texting ,name="texting"),
     path('text/ip', views.ip_check ,name="ip"),
     re_path(r'', include(routing.websocket_urlpattern)),

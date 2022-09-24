26c3c929cbd495b4219dfa4c716fad5929ef076b
Ayush Jindal(Jindalayush987@gmal.com) <94276821+Qwer278@users.noreply.github.com>
2022-09-24 16:07:40 +0530

    first commit

--- service/consumers.py
+++ service/consumers.py
@@ -12,54 +12,54 @@ import datetime
 import socket
 
 class ChatConsumer(WebsocketConsumer):
-    def websocket_connect(self,event):
+    def connect(self):
         print('CONNECTED>>>>>>>>')
-        
-        hn=socket.gethostname()
-        ip=socket.gethostbyname(hn)
-        me=ip
-        print(ip)
-        room.objects.create(host1=me,host2=me).save()
-        try:
-            hosting.objects.get(ip=me)
-
-        except hosting.DoesNotExist:
-            ip_address = hosting.objects.create(status=1,ip=me, pub_date=datetime.datetime.now())
-            ip_address.save()
-
-        try:
-            ip_list=hosting.objects.filter(status=1)
-            ip_list=ip_list.exclude(ip=me)
-            if not ip_list:
-                time.sleep(5)
-            if ip_list:
-                
-                ip2=random.choice(ip_list)
-                # print('other user >>>>>>>>>>>>>>>>>>>>>>',ip2.ip)
-                other_user=ip2
-                try:
-                    room.objects.filter(host1=me).delete()
-                    room.objects.filter(host2=me).delete()
-                    room.objects.filter(host1=other_user).delete()
-                    room.objects.filter(host2=other_user).delete()
-                except:
-                    print('Not USER Found>>>>>>')
-            
-                room.objects.create(host1=me,host2=ip2.ip).save()
-        except:
-            pass
-        # self.room='1'
-        self.room=str(room.objects.only('room_id').get(host1=me).room_id)
-        # print(self.room)
-        # self.send({
-        #     "type":"websocket.accept"
-        # })
-        # self.room='sss'
-
-        async_to_sync(self.channel_layer.group_add)(
-            self.room,
-            self.channel_name)
         self.accept()
+        # hn=socket.gethostname()
+        # ip=socket.gethostbyname(hn)
+        # me=ip
+        # print(ip)
+        # room.objects.create(host1=me,host2=me).save()
+        # try:
+        #     hosting.objects.get(ip=me)
+
+        # except hosting.DoesNotExist:
+        #     ip_address = hosting.objects.create(status=1,ip=me, pub_date=datetime.datetime.now())
+        #     ip_address.save()
+
+        # try:
+        #     ip_list=hosting.objects.filter(status=1)
+        #     ip_list=ip_list.exclude(ip=me)
+        #     if not ip_list:
+        #         time.sleep(5)
+        #     if ip_list:
+                
+        #         ip2=random.choice(ip_list)
+        #         # print('other user >>>>>>>>>>>>>>>>>>>>>>',ip2.ip)
+        #         other_user=ip2
+        #         try:
+        #             room.objects.filter(host1=me).delete()
+        #             room.objects.filter(host2=me).delete()
+        #             room.objects.filter(host1=other_user).delete()
+        #             room.objects.filter(host2=other_user).delete()
+        #         except:
+        #             print('Not USER Found>>>>>>')
+            
+        #         room.objects.create(host1=me,host2=ip2.ip).save()
+        # except:
+        #     pass
+        # # self.room='1'
+        # self.room=str(room.objects.only('room_id').get(host1=me).room_id)
+        # # print(self.room)
+        # # self.send({
+        # #     "type":"websocket.accept"
+        # # })
+        # # self.room='sss'
+
+        # async_to_sync(self.channel_layer.group_add)(
+        #     self.room,
+        #     self.channel_name)
+        # self.accept()
         # print(f'[{self.channel_layer}]','you are connected')
     
     def chat_message(self,event):

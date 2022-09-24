74e910de1323081b57436b56868c4e708b57f7ac
Ayush Jindal(Jindalayush987@gmal.com) <94276821+Qwer278@users.noreply.github.com>
2022-09-24 16:03:46 +0530

    first commit

--- service/routing.py
+++ service/routing.py
@@ -1,8 +1,8 @@
 from django.urls import re_path,path
 # from django.conf.urls import url
 from . import consumers
-from .consumers import ChatConsumer
+from . import consumers
 
 websocket_urlpattern=[
-    re_path(r'^text/ws/$',ChatConsumer.as_asgi()),
+    re_path(r'text/ws/$',consumers.ChatConsumer.as_asgi()),
 ]   
